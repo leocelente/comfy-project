bef8d317688166ddcd6c3182d0a2d011544d5ccd
Leonardo Celente <leocelente@gmail.com>
2021-06-05 14:58:38 -0300

    Added DearImgui to project

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,88 @@
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 #include "module.hpp"
+#include <GL/glew.h>// Initialize with glewInit()
 #include <ctre.hpp>
 #include <fmt/core.h>
 #include <spdlog/spdlog.h>
 
+#include <GLFW/glfw3.h>
 
 int main(int const argc, char *argv[]) {
+
+	// Setup window
+	glfwSetErrorCallback(
+	  [](int e, char const *desc) { fmt::print("Error [{}]: {}", e, desc); });
+	if (!glfwInit()) return 1;
+
+	const char *glsl_version = "#version 130";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+	GLFWwindow *window =
+	  glfwCreateWindow(1280, 720, "Dear ImGui - Conan", NULL, NULL);
+	if (window == NULL) return 1;
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);// Enable vsync
+	bool err = glewInit() != GLEW_OK;
+
+	if (err) {
+		fprintf(stderr, "Failed to initialize OpenGL loader!\n");
+		return 1;
+	}
+	int screen_width, screen_height;
+	glfwGetFramebufferSize(window, &screen_width, &screen_height);
+	glViewport(0, 0, screen_width, screen_height);
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO &io = ImGui::GetIO();
+	// Setup Platform/Renderer bindings
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(glsl_version);
+	// Setup Dear ImGui style
+	ImGui::StyleColorsDark();
+
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// feed inputs to dear imgui, start new frame
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		// render your GUI
+		ImGui::Begin("Triangle Position/Color");
+		static float rotation = 0.0;
+		ImGui::SliderFloat("rotation", &rotation, 0, 2 * 3.1415f);
+		static float translation[] = { 0.0, 0.0 };
+		ImGui::SliderFloat2("position", translation, -1.0, 1.0);
+		static float color[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
+		// pass the parameters to the shader
+		// color picker
+		ImGui::ColorEdit3("color", color);
+		// multiply triangle's color with this color
+		ImGui::End();
+
+		// Render dear imgui into screen
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		int display_w, display_h;
+		glfwGetFramebufferSize(window, &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+		glfwSwapBuffers(window);
+	}
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 
 	if (test()) fmt::print("Hello There!\n");
 	return 0;
